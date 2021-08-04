8f00967f123fd779245b7197a36ca453fc77ef79
xm4dn355 <xm4dn355x@gmail.com>
2021-08-04 21:51:56 +0300

    add Item model

--- firstDjango/MainApp/models.py
+++ firstDjango/MainApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models import Model, CharField, PositiveIntegerField, TextField
+
 
 # Create your models here.
+
+class Item(Model):
+    name = CharField(max_length=100)
+    quantity = PositiveIntegerField()
+    description = TextField(default="")
+
+    def __repr__(self):
+        return f'Item: {self.name}'
+
+    def __str__(self):
+        return f'Item: {self.name} q:{self.quantity}'
